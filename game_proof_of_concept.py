c74a3f6772540204bf3852221b2833d23a49052a
Gargathar <quinn.m.shields@gmail.com>
2025-07-24 15:53:09 -0400

    Player movement added

--- game_proof_of_concept.py
+++ game_proof_of_concept.py
@@ -6,7 +6,7 @@ class Cell:
 	def __init__(self, y:int, x:int):
 		self.y:int = y
 		self.x:int = x
-		self.wall:bool = True
+		self.wall:bool = False
 		self.bound = None
 	
 	def __repr__(self):
@@ -22,37 +22,121 @@ class Grid:
 
 	def __init__(self):
 
-		self.GRIDLEN = 10 # this assumes the grid is a uniform square
+		self.GRIDLEN = 11 # this assumes the grid is a uniform square
 
 		self.grid = []
 
-		for i in range(self.GRIDLEN+1):
+		for i in range(self.GRIDLEN):
 			self.grid.append([])
-			for j in range(self.GRIDLEN+1):
+			for j in range(self.GRIDLEN):
 				
 				temp_cell = Cell(i,j)
 				self.grid[i].append(temp_cell)
 
-				if i == 0 and j == floor(self.GRIDLEN/2):
-					temp_cell.bound = "north"
+				if i == 0:
+					if j == floor((self.GRIDLEN-1)/2):
+						temp_cell.bound = "north"
+					else:
+						temp_cell.wall = True
 				
-				elif i == floor(self.GRIDLEN/2) and j == 0:
-					temp_cell.bound = "west"
+				elif j == 0:
+					if i == floor((self.GRIDLEN-1)/2):
+						temp_cell.bound = "west"
+					else:
+						temp_cell.wall = True
 				
-				elif i == self.GRIDLEN and j == floor(self.GRIDLEN/2):
-					temp_cell.bound = "south"
+				elif i == self.GRIDLEN-1: 
+					if j == floor((self.GRIDLEN-1)/2):
+						temp_cell.bound = "south"
+					else:
+						temp_cell.wall = True
+
 				
-				elif i == floor(self.GRIDLEN/2) and j == self.GRIDLEN:
-					temp_cell.bound = "east"
+				elif j == (self.GRIDLEN-1):
+					if i == floor((self.GRIDLEN-1)/2):
+						temp_cell.bound = "east"
+					else:
+						temp_cell.wall = True
 						
 
 
-
+	def print_grid(self):
 		for row in self.grid:
 			for cell in row:
 				print(cell, end="")
 			print()
 
 
+class Player:
+
+	def __init__(self, grid:Grid):
+		self.grid:Grid = grid
+		self.y:int = 0
+		self.x:int = 5
+	
+	def display_clear(self):
+		print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+	def draw(self):
+		for vert_adj in range(-2,3):
+			if (self.y + vert_adj) < 0:
+				print("  ")
+				continue
+			if (self.y + vert_adj) > self.grid.GRIDLEN-1:
+				print("  ")
+				continue
+			for hor_adj in range(-2,3):
+				if (self.x + hor_adj) < 0:
+					print("  ", end="")
+					continue
+				if (self.x + hor_adj) > self.grid.GRIDLEN-1:
+					print("  ", end="")
+					continue
+
+				if hor_adj == 0 and vert_adj == 0:
+					print("7∆", end="")
+				else:
+					print(f"{self.grid.grid[self.y + vert_adj][self.x + hor_adj].__repr__()}", end="")
+			print()
+	
+	def move(self):
+		stay_going = True
+		while stay_going:
+			self.display_clear()
+			self.draw()
+			match input("> ").lower():
+				case 'w':
+					option_y = self.y - 1
+					option_x = self.x
+				case 's':
+					option_y = self.y + 1
+					option_x = self.x
+				case 'a':
+					option_x = self.x - 1
+					option_y = self.y
+				case 'd':
+					option_x = self.x + 1
+					option_y = self.y
+				case 'exit':
+					stay_going = False
+					break
+				case _:
+					continue
+			try:
+				if self.grid.grid[option_y][option_x].wall == True:
+					continue
+				else:
+					if option_x >= 0 and option_y >= 0:
+						self.x = option_x
+						self.y = option_y
+			except:
+				continue
+				
+				
+
+
+
 if __name__ == "__main__":
 	g = Grid()
+	p = Player(g)
+	p.move()

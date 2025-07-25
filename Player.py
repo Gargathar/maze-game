9ca2121114653d090f82fc151ab912dc70c1e402
Gargathar <quinn.m.shields@gmail.com>
2025-07-25 16:52:42 -0400

    Got rid of globals, made a Player class

--- Player.py
+++ Player.py
@@ -3,131 +3,139 @@ import time
 # Create a new scene
 scene = w2d.Scene(width=800, height=600, background=(0.9, 0.9, 1.0), title="My Scene")
 animate = w2d.animate 
-# Create a new square sprite for the player
-player = scene.layers[1].add_rect(
-    width=50,
-    height=50,
-    pos=(400, 300),
-    color=(1, 0, 0),  # Red color
-)
 
 
-attacking = 0  
-direction = 0
-# Define movement functions
-def move_up():
-    player.y -= 10
-    direction = 90
-
-def move_down():
-    player.y += 10
-    direction = 270
-
-def move_left():
-    player.x -= 10
-    direction = 0
-
-def move_right():
-    player.x += 10
-    direction = 180
-  # Remove the sword after the attack
-def attack():
-    Sword = scene.layers[0].add_rect(
-    width=10,
-    height=50,
-    pos=(400, 300),
-    color=(0, 0, 0),
-      # Black color for the sword
+class Player:
     
-)
+    def __init__(self):
+        
+        # Create a new square sprite for the Player
+        self.sprite = scene.layers[1].add_rect(
+            width=50,
+            height=50,
+            pos=(400, 300),
+            color=(1, 0, 0),  # Red color
+        )
 
-    def done_swing():
-        attacking = 0
-        Sword.delete()
-    if direction == 0:  # Player is facing left
-        Sword.pos = (player.pos - (25, 0))
-        animate(Sword, tween='linear', duration=0.3, angle=-3, on_finished=done_swing)
-        
-    elif direction == 90:  # Player is facing up
-        Sword.pos = (player.pos + (0, 25))
-        animate(Sword, tween='linear', duration=0.3, angle=3, on_finished=done_swing )
-        
-    elif direction == 180:  # Player is facing right
-        Sword.pos = (player.pos + (25, 0))
-        animate(Sword, tween='linear', duration=0.3, angle=3, on_finished=done_swing ) 
-        
-    elif direction == 270:  # Player is facing down
-        Sword.pos = (player.pos - (0, 25))
-        animate(Sword, tween='linear', duration=0.3, angle=-3, on_finished=done_swing )
-        
-      # Position the sword at the player's position
-    # for i in range(6):
-    #     Sword.angle += (6)
-    #     time.sleep(0.1)
-    # time.sleep(5)
+        # Set flags for key presses
+        self.up_pressed = False
+        self.down_pressed = False
+        self.left_pressed = False
+        self.right_pressed = False
+        self.space_pressed = False
+
+        self.attacking = 0  
+        self.direction = 0
+
+    # Define movement functions
+    def move_up(self):
+        self.sprite.y -= 10
+        self.direction = 90
+
+    def move_down(self):
+        self.sprite.y += 10
+        self.direction = 270
+
+    def move_left(self):
+        self.sprite.x -= 10
+        self.direction = 0
+
+    def move_right(self):
+        self.sprite.x += 10
+        self.direction = 180
+
+
+
     # Remove the sword after the attack
-    attacking = 0  # Reset attacking state
-    
+    def attack(self):
+        self.Sword = scene.layers[0].add_rect(
+        width=10,
+        height=50,
+        pos=(400, 300),
+        color=(0, 0, 0),
+        # Black color for the sword
+        )
 
-# Set flags for key presses
-up_pressed = False
-down_pressed = False
-left_pressed = False
-right_pressed = False
-space_pressed = False
+    def done_swing(self):
+        self.attacking = 0
+        self.Sword.delete()
+        if self.direction == 0:  # Player is facing left
+            self.Sword.pos = (self.sprite.pos - (25, 0))
+            animate(self.Sword, tween='linear', duration=0.3, angle=-3, on_finished=self.done_swing)
+            
+        elif self.direction == 90:  # Player is facing up
+            self.Sword.pos = (self.sprite.pos + (0, 25))
+            animate(self.Sword, tween='linear', duration=0.3, angle=3, on_finished=self.done_swing)
+            
+        elif self.direction == 180:  # Player is facing right
+            self.Sword.pos = (self.sprite.pos + (25, 0))
+            animate(self.Sword, tween='linear', duration=0.3, angle=3, on_finished=self.done_swing) 
+            
+        elif self.direction == 270:  # Player is facing down
+            self.Sword.pos = (self.sprite.pos - (0, 25))
+            animate(self.Sword, tween='linear', duration=0.3, angle=-3, on_finished=self.done_swing)
+            
+        # Position the sword at the sprite's position
+        # for i in range(6):
+        #     Sword.angle += (6)
+        #     time.sleep(0.1)
+        # time.sleep(5)
+        # Remove the sword after the attack
+        self.attacking = 0  # Reset attacking state
+
+
+player = Player()
 # Bind movement functions to arrow keys
 @w2d.event
 def on_key_down(key):
-    global up_pressed, down_pressed, left_pressed, right_pressed , space_pressed
-    if attacking == 1:
+    if player.attacking == 1:
         pass
-    elif attacking == 0:
+    elif player.attacking == 0:
         if key == w2d.keys.UP:
-            up_pressed = True
+            player.up_pressed = True
         elif key == w2d.keys.DOWN:
-            down_pressed = True
+            player.down_pressed = True
         elif key == w2d.keys.LEFT:
-            left_pressed = True
+            player.left_pressed = True
         elif key == w2d.keys.RIGHT:
-            right_pressed = True
+            player.right_pressed = True
         elif key == w2d.keys.SPACE:
-            attack()
+            player.attack()
     else:
-        print(attacking)
+        print(player.attacking)
 
 
 
 @w2d.event
 def on_key_up(key):
-    global up_pressed, down_pressed, left_pressed, right_pressed, space_pressed
-    if attacking == 1:
+    if player.attacking == 1:
         pass
-    elif attacking == 0:
+    elif player.attacking == 0:
         if key == w2d.keys.UP:
-            up_pressed = False
+            player.up_pressed = False
         elif key == w2d.keys.DOWN:
-            down_pressed = False
+            player.down_pressed = False
         elif key == w2d.keys.LEFT:
-            left_pressed = False
+            player.left_pressed = False
         elif key == w2d.keys.RIGHT:
-            right_pressed = False
+            player.right_pressed = False
     else:
-        print(attacking)
+        print(player.attacking)
 
 def update():
-    global up_pressed, down_pressed, left_pressed, right_pressed, space_pressed
-    if up_pressed:
-        move_up()
-    if down_pressed:
-        move_down()
-    if left_pressed:
-        move_left()
-    if right_pressed:
-        move_right()
+    if player.up_pressed:
+        player.move_up()
+    if player.down_pressed:
+        player.move_down()
+    if player.left_pressed:
+        player.move_left()
+    if player.right_pressed:
+        player.move_right()
     w2d.clock.schedule(update, 1/60)
 
 
 
 # Run the scene
+
 update()
 w2d.run()

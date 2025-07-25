a6c486f464fa29d8dbb90a6adacd902d8314cbe2
Gargathar <quinn.m.shields@gmail.com>
2025-07-25 13:13:01 -0400

    Chunk generation functions methods of Chunk class

--- game_proof_of_concept.py
+++ game_proof_of_concept.py
@@ -17,19 +17,19 @@ class Cell:
 			return ".."
 
 
-class Grid:
+class Chunk:
 
 	def __init__(self):
 
-		self.GRIDLEN = 30 # this assumes the grid is a uniform square
-		mid = self.GRIDLEN//2
+		self.CHUNKLEN = 30 # this assumes the grid is a uniform square
+		mid = self.CHUNKLEN//2
 
 		self.dir = [[1,0],[-1,0],[0,1],[0,-1]]
 		self.grid = []
 
-		for i in range(self.GRIDLEN+1):
+		for i in range(self.CHUNKLEN+1):
 			self.grid.append([])
-			for j in range(self.GRIDLEN+1):
+			for j in range(self.CHUNKLEN+1):
 				
 				temp_cell = Cell(i,j)
 				self.grid[i].append(temp_cell)
@@ -40,35 +40,43 @@ class Grid:
 				elif i == mid and j == 0:
 					temp_cell.bound = "west"
 				
-				elif i == self.GRIDLEN and j == mid:
+				elif i == self.CHUNKLEN and j == mid:
 					temp_cell.bound = "south"
 				
-				elif i == mid and j == self.GRIDLEN:
+				elif i == mid and j == self.CHUNKLEN:
 					temp_cell.bound = "east"
-	def print_grid(self):
+
+	
+
+	def dfs(self, i:int, j:int, vis:list[list[bool]]): # helps buils the maze
+		shuffle(self.dir)
+		vis[i][j] = True
+		self.grid[2*i+1][2*j+1].wall = False
+		
+		mid = self.CHUNKLEN//2
+		for d in self.dir:
+			newI, newJ = i + d[0], j + d[1]
+			if newI>=0 and newI < mid and newJ>=0 and newJ < mid and not vis[newI][newJ]:
+				self.grid[2*i+1 + d[0]][2*j+1 + d[1]].wall = False
+				self.dfs(newI, newJ, vis)
+
+	def build_maze(self): # involved in dfs building
+		mid = self.CHUNKLEN//2
+		vis = [[False for _ in range(mid)] for _ in range(mid)]
+		self.dfs(0, 0, vis)
+
+
+
+
+	def print_chunk(self):
 		for row in self.grid:
 			for cell in row:
 				print(cell, end="")
 			print()
 
-def dfs(g:Grid, i:int, j:int, vis:list[list[bool]]):
-    shuffle(g.dir)
-    vis[i][j] = True
-    g.grid[2*i+1][2*j+1].wall = False
-    
-    mid = g.GRIDLEN//2
-    for d in g.dir:
-        newI, newJ = i + d[0], j + d[1]
-        if newI>=0 and newI < mid and newJ>=0 and newJ < mid and not vis[newI][newJ]:
-            g.grid[2*i+1 + d[0]][2*j+1 + d[1]].wall = False
-            dfs(g, newI, newJ, vis)
 
-def build_maze(g:Grid):
-    mid = g.GRIDLEN//2
-    vis = [[False for _ in range(mid)] for _ in range(mid)]
-    dfs(g, 0, 0, vis)
 
 if __name__ == "__main__":
-	g = Grid()
-	build_maze(g)
-	g.print_grid()
+	g = Chunk()
+	g.build_maze()
+	g.print_chunk()

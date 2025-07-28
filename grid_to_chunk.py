8ac8e49084369735e7c57a1fc8e75deefc55170b
maxdu25 <maxwell.q.du@gmail.com>
2025-07-28 10:26:40 -0400

    Added script to convert 2d array into chunk

---
+++ grid_to_chunk.py
@@ -0,0 +1,18 @@
+from chunk_generation import Cell, Chunk
+
+def convert(level:list[list[str]]) -> Chunk:
+    chunk = Chunk()
+    size = len(level)
+    chunk.grid = []
+    for i in range(size):
+        chunk.grid.append([])
+        for j in range(size):
+            cur = Cell(i,j)
+            chunk.grid[i].append(cur)
+            if level[i][j] == '..':
+                cur.wall = False
+    return chunk
+
+if __name__ == '__main__':
+    ch = convert([['..','##'],['##','..']])
+    ch.print_chunk()
